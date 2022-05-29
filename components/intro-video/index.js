3fd18cef105f4df98f06689aa4ce4a6f17472f13
Pim de Wit <pim@pdw.io>
2022-05-29 12:44:48 +0100

    feat(IntroVideo): remove lazyload

--- components/intro-video/index.js
+++ components/intro-video/index.js
@@ -1,20 +1,17 @@
-import LazyLoad from "react-lazyload";
 import * as style from "./style.module.css";
 
 export function IntroVideo({ url }) {
   return (
-    <LazyLoad height={576}>
-      <video
-        src={url}
-        muted
-        loop
-        preload="none"
-        playsInline={true}
-        autoPlay={true}
-        width={1024}
-        height={576}
-        className={style.video}
-      />
-    </LazyLoad>
+    <video
+      src={url}
+      muted
+      loop
+      preload="none"
+      playsInline={true}
+      autoPlay={true}
+      width={1024}
+      height={576}
+      className={style.video}
+    />
   );
 }
